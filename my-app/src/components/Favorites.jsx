a62e5ed435827a044e4ef11fe8123d8c7f9dc030
Edvin <you@example.com>
2021-11-25 19:28:11 +0200

    Logic updated

--- my-app/src/components/Favorites.jsx
+++ my-app/src/components/Favorites.jsx
@@ -1,13 +1,42 @@
 import React from "react";
+import Joke from "./Joke";
 
 
-class Favorites extends React.Component{
-    render(){
-        return (
-            <div className="favorites">
-                <h1>Favorites link</h1>
-            </div>
-        )
+class Favorites extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            savedList: [],
+        }
+    }
+    componentDidMount() {
+        if (localStorage.length) {
+            this.setState((state) => ({
+                savedList: state.savedList.concat(JSON.parse(localStorage.getItem('favoritesJokes')))
+            }))
+        }
+    }
+
+    render() {
+        console.log(this.state.savedList);
+        if (this.state.savedList.length) {
+
+
+            return (
+                <div className="favorites">
+                    <h1>Favorites list</h1>
+                    <div className="row">
+                        {this.state.savedList.map((j)=> <Joke id={j.id} title={j.joke}/>)}
+                    </div>
+                </div>
+            )
+        }else {
+            return(
+                
+                    <p>Issaugotu filmu nera</p>
+                
+            )
+        }
     }
 }
 
--- my-app/src/components/Joke.jsx
+++ my-app/src/components/Joke.jsx
@@ -1,18 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom"
+let favoritesList = [];
 
-class Joke extends React.Component{
-    constructor(props){
-        super(props);
+class Joke extends React.Component {
+    constructor() {
+        super();
+        this.handleAddFavorites = this.handleAddFavorites.bind(this)
     }
-    render(){
+
+    handleAddFavorites() {
+        let obj = {};
+        obj["id"] = this.props.id;
+        obj["joke"] = this.props.title;
+        obj["categories"] = [];
+        favoritesList.push(obj);
+        localStorage.setItem("favoritesJokes", JSON.stringify(favoritesList))
+    }
+    render() {
         return (
-            <div className="card" style={{width: "18rem"}}>
-    <div className="card-body">
-        <h5 className="card-title">{this.props.id}</h5>
-        <p className="card-text">{this.props.title}</p>
-        <a href="#" className="card-link">Add to favorites</a>
-    </div>
-</div>
+            <div className="card" style={{ width: "18rem" }}>
+                <div className="card-body">
+                    <h5 className="card-title">{this.props.id}</h5>
+                    <p className="card-text">{this.props.title}</p>
+                    <p>{this.props.status}</p>
+                    {this.props.status === "notSaved" ? (<Link to="/favorites" className="btn btn-primary" onClick={this.handleAddFavorites}>Add to favorites </Link>) : null}
+
+                </div>
+            </div>
         )
     }
 }
